5a945cf88f0963709130a50bd8426d5a0217f137
nick-simone <nick.simone100@gmail.com>
2021-02-28 18:31:33 -0500

    created basic swift app

---
+++ Key Transposer/Key_TransposerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Key_TransposerApp.swift
+//  Key Transposer
+//
+//  Created by Nick Simone on 2021-02-28.
+//
+
+import SwiftUI
+
+@main
+struct Key_TransposerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
