a8a41040cc205f8a7bfe89144c293dad868083b5
nick-simone <nick.simone100@gmail.com>
2021-03-01 15:36:03 -0500

    recovered files

---
+++ Key Transposer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Key Transposer
+//
+//  Created by Nick Simone on 2021-03-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
