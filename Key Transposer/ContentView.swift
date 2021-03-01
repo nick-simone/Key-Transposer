89f8c7fd424232dd06c3c15a9c4f9effc3e444fe
nick-simone <nick.simone100@gmail.com>
2021-03-01 16:30:20 -0500

    started interface setup

--- Key Transposer/ContentView.swift
+++ Key Transposer/ContentView.swift
@@ -9,11 +9,65 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Key Transposer")
-            .font(.largeTitle)
-            .fontWeight(.heavy)
-            .multilineTextAlignment(.center)
-            .padding()
+        VStack {
+            Text("Key Transposer")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .multilineTextAlignment(.center)
+                .padding()
+            Label("Chord Progression", systemImage: "")
+            HStack {
+                Picker(selection: .constant(1)/*@END_MENU_TOKEN@*/, label: /*@START_MENU_TOKEN@*/Text("Picker")) {
+                    Text("A").tag(1)
+                    Text("B").tag(2)
+                    Text("C").tag(2)
+                    Text("D").tag(2)
+                    Text("E").tag(2)
+                    Text("F").tag(2)
+                    Text("G").tag(2)
+                }.frame(width: 50 * UIScreen.main.bounds.width/100)
+                Picker(selection: .constant(1)/*@END_MENU_TOKEN@*/, label: /*@START_MENU_TOKEN@*/Text("Picker")) {
+                    Text("A").tag(1)
+                    Text("B").tag(2)
+                    Text("C").tag(2)
+                    Text("D").tag(2)
+                    Text("E").tag(2)
+                    Text("F").tag(2)
+                    Text("G").tag(2)
+                }
+                .frame(width: 50 * UIScreen.main.bounds.width/100)
+                
+            }
+            Spacer().frame(height: 20)
+            Label("Desired Key", systemImage: "")
+            Spacer().frame(height: 20)
+            // Key Section - Buttons
+            HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 20, content: {
+
+                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("A")
+                }
+                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("B")
+                }
+                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("C")
+                }
+                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("D")
+                }
+                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("E")
+                }
+                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("F")
+                }
+                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("G")
+                }
+            })
+            
+        }
     }
 }
 
